d081283b258f8508e9ee5cc8ad68047017cd676e
gusdev0258 <16326176760@edu.udesc.br>
2023-01-24 16:23:24 -0300

    Continuando com os projetos

---
+++ projeto-javascript/script/script-aula.js
@@ -0,0 +1,18 @@
+// const imgs = document.querySelectorAll('img');
+// console.log(imgs);
+// const imagem = document.querySelectorAll('[src*=imagem]');
+// console.log(imagem);
+// const linksInternos = document.querySelectorAll('[href^="#"]');
+// console.log(linksInternos);
+// const titulinho = document.querySelector('.animais-descricao h2:first-child');
+// console.log(titulinho);
+// const ultimoP = document.querySelector('.footer p');
+// console.log(ultimoP);
+// const pS = document.querySelectorAll('p');
+// console.log(pS[pS.length - 1]);
+// const paragrafos = document.querySelectorAll('p');
+// console.log(paragrafos);
+// paragrafos.forEach((p) => {
+//   console.log(p.innerText);
+// });
+
