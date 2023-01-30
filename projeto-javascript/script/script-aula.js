629a5e345eb3519aea20106acbf728ad6a9cd843
gusdev0258 <16326176760@edu.udesc.br>
2023-01-30 05:35:05 -0300

    continuando o projeto do site

--- projeto-javascript/script/script-aula.js
+++ projeto-javascript/script/script-aula.js
@@ -15,4 +15,26 @@
 // paragrafos.forEach((p) => {
 //   console.log(p.innerText);
 // });
+// const menu = document.querySelectorAll('.menu ul li a');
+// console.log(menu);
+// menu.forEach((item) => {
+//   item.classList.add('ativo');
+  // if(item.hasAttribute('alt')){
+  //   console.log('bunda')
+  // }else{
+  //   console.log('pinto')
+  // }
+//   let itemHref = item.getAttribute('href');
+//   if(!itemHref.includes('#')){
+//     let item2 = item.getAttribute('href');
+//     item.setAttribute('href', "https://www.google.com.br");
+//   }
+// }
+// );
+// console.log(menu.length);
+// for(let i = 1; i< menu.length; i++){
+//   let itemMenu = menu.item(i);
+//   itemMenu.classList.remove('ativo');
+// }
+
 
--- projeto-javascript/script/script.js
+++ projeto-javascript/script/script.js
@@ -1,1 +1,17 @@
- 
+const tabMenu = document.querySelectorAll(".js-tabmenu li");
+const tabContent = document.querySelectorAll(".js-tabcontent section");
+
+if (tabMenu.length && tabContent.length) {
+  tabContent[0].classList.add("ativo");
+  function activeTab(index) {
+    tabContent.forEach((tabItem) => {
+      tabItem.classList.remove("ativo");
+    });
+    tabContent[index].classList.add("ativo");
+  }
+  tabMenu.forEach((menuItem, index) => {
+    menuItem.addEventListener("click", () => {
+      activeTab(index);
+    });
+  });
+}
