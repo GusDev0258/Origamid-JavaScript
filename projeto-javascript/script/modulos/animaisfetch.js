d0d1dd003ea5f9fb5e7a7b3afc887c255cb05646
gusdev0258 <16326176760@edu.udesc.br>
2023-02-24 15:09:37 -0300

    add: webpack

--- projeto-javascript/script/modulos/animaisfetch.js
+++ projeto-javascript/script/modulos/animaisfetch.js
@@ -1,10 +1,16 @@
 import initAnimarNumeros from "./numeros.js";
+
 export default function initAnimaisFetch() {
   async function fetchAnimais(url) {
     const animaisResponse = await fetch(url);
     const responseJSON = await animaisResponse.json();
-    const numerosGrid = document.querySelector('.numeros-grid');
- 
+    const numerosGrid = document.querySelector(".numeros-grid");
+    function createAnimal(animal) {
+      const div = document.createElement("div");
+      div.classList.add("numero-animal");
+      div.innerHTML = `<h3>${animal.especie}</h3><span data-numero>${animal.total}</span>`;
+      return div;
+    }
     responseJSON.forEach((animal) => {
       const divAnimal = createAnimal(animal);
       numerosGrid.appendChild(divAnimal);
@@ -14,11 +20,4 @@ export default function initAnimaisFetch() {
   fetchAnimais(
     "http://127.0.0.1:5500/projeto-javascript/script/modulos/animaisapi.json"
   );
-  function createAnimal(animal) {
-    const div = document.createElement("div");
-    div.classList.add("numero-animal");
-    div.innerHTML = `<h3>${animal.especie}</h3><span data-numero>${animal.total}</span>`;
-    return div;
-  }
-  
 }
--- projeto-javascript/script/modulos/dropdown.js
+++ projeto-javascript/script/modulos/dropdown.js
@@ -1,11 +1,7 @@
 import outsideClick from "./outsideClick.js";
+
 export default function initDropdownMenu() {
   const subMenus = document.querySelectorAll("[data-dropdown]");
-  subMenus.forEach((menu) => {
-    ["touchstart", "click"].forEach((userEvent) => {
-      menu.addEventListener(userEvent, handleClick);
-    });
-  });
   function handleClick(event) {
     event.preventDefault();
     this.classList.add("active");
@@ -13,4 +9,9 @@ export default function initDropdownMenu() {
       this.classList.remove("active");
     });
   }
+  subMenus.forEach((menu) => {
+    ["touchstart", "click"].forEach((userEvent) => {
+      menu.addEventListener(userEvent, handleClick);
+    });
+  });
 }
